5ef3c282402fa66bc6556a5409f8e49cd4216918
Dmytro Shapovalov <dshtorm@gmail.com>
2020-04-29 17:48:40 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
       .library(name: "HyperTrack", targets: ["HyperTrack"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/groue/GRDB.swift.git", from: "4.4.0")
+    .package(name: "GRDB", url: "https://github.com/groue/GRDB.swift.git", from: "4.4.0"),
   ],
   targets: [
     .target(
