ae689ac4e90da37fcbbdb15d8b1a0c910fc2e803
Dmytro Shapovalov <dshtorm@gmail.com>
2020-09-02 10:42:59 +0200

    qq

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "HyperTrack",
+    name: "HyperTrackSPM",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "HyperTrack",
-            targets: ["HyperTrack"]),
+            name: "HyperTrackSPM",
+            targets: ["HyperTrackSPM"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,6 +21,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+      .target(
+          name: "HyperTrackSPM",
+          dependencies: ["HyperTrack", "GRDB"]
+      ),
       .binaryTarget(name: "HyperTrack",
                     url: "https://s3-us-west-2.amazonaws.com/sdk-config.hypertrack.com/HyperTrack.xcframework.zip",
                     checksum: "60a5118aed4da1d95972dfccf26b5c052ba0ef5cdcd7975b8b4eab8154a696ca")
