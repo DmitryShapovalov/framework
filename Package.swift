71112acb9fb0f75ddc1fd726fe332c25772316ff
Dmytro Shapovalov <dshtorm@gmail.com>
2020-09-02 11:07:20 +0200

    new framework

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "HyperTrackSPM",
+    name: "HyperTrackTestFramework",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "HyperTrackSPM",
-            targets: ["HyperTrackSPM"]),
+            name: "HyperTrackTestFramework",
+            targets: ["HyperTrackTestFramework"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,12 +21,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-      .target(
-          name: "HyperTrackSPM",
-          dependencies: ["HyperTrack"]
-      ),
-      .binaryTarget(name: "HyperTrack",
-                    url: "https://s3-us-west-2.amazonaws.com/sdk-config.hypertrack.com/HyperTrack.xcframework.zip",
-                    checksum: "60a5118aed4da1d95972dfccf26b5c052ba0ef5cdcd7975b8b4eab8154a696ca")
+//      .binaryTarget(name: "HyperTrack",
+//                    url: "https://s3-us-west-2.amazonaws.com/sdk-config.hypertrack.com/HyperTrack.xcframework.zip",
+//                    checksum: "60a5118aed4da1d95972dfccf26b5c052ba0ef5cdcd7975b8b4eab8154a696ca")
+      .binaryTarget(name: "HyperTrackTestFramework", path: "HyperTrackTestFramework.xcframework")
     ]
 )
