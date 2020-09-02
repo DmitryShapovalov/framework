912b1bbd8dbc44e227a43516dbe83bf7b19b978b
Dmytro Shapovalov <dshtorm@gmail.com>
2020-09-02 10:44:39 +0200

    sss

--- Package.swift
+++ Package.swift
@@ -16,14 +16,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/groue/GRDB.swift.git", from: "4.4.0"),
+//        .package(url: "https://github.com/groue/GRDB.swift.git", from: "4.4.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
       .target(
           name: "HyperTrackSPM",
-          dependencies: ["HyperTrack", "GRDB"]
+          dependencies: ["HyperTrack"]
       ),
       .binaryTarget(name: "HyperTrack",
                     url: "https://s3-us-west-2.amazonaws.com/sdk-config.hypertrack.com/HyperTrack.xcframework.zip",
