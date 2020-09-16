85f1efd7ab672b4ec60307a534c2f4bce7c2d3f5
Dmytro Shapovalov <dshtorm@gmail.com>
2020-09-16 11:06:01 +0200

    0.26

--- Package.swift
+++ Package.swift
@@ -16,6 +16,11 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-      .binaryTarget(name: "HyperTrackTestFramework", path: "HyperTrackTestFramework.xcframework")
+      .binaryTarget(
+          name: "HyperTrackTestFramework",
+          url: "https://github.com/DmitryShapovalov/framework/releases/download/4.0.26/HyperTrackTestFramework.xcframework.zip",
+          checksum: "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e"
+      )
     ]
 )
+
