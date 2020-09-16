3ee6646ac845fa21775be281a9167158a2b250ef
Dmytro Shapovalov <dshtorm@gmail.com>
2020-09-16 11:16:57 +0200

    asdad

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
       .binaryTarget(
           name: "HyperTrackTestFramework",
-          url: "https://github.com/DmitryShapovalov/framework/releases/download/4.0.27/HyperTrackTestFramework.zip",
+          url: "https://s3-us-west-2.amazonaws.com/sdk-config.hypertrack.com/HyperTrackTestFramework.zip",
           checksum: "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e"
       )
     ]
